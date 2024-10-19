5b94e80f54082cb3597ddd2e9e3c60edc31013ff
kourosh af <kouroshaf81@gmail.com>
2024-10-20 00:38:13 +0330

    bug fix

--- src/Components/Header/UserAction.jsx
+++ src/Components/Header/UserAction.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const UserAction = () => {
     return (
         <div className="user-action">
-            <Link href='/' className="login-signup-btn">ورود | ثبتنام</Link>
+            <Link href='/' className="login-signup-btn">ورود | ثبت نام</Link>
             <Link href='/' className="add-ads-btn">ثبت آگهی</Link>
         </div>
     )
